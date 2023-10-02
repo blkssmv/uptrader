42dbbebf3189f752a9b0ed34039d76a3cfa42ec3
bekzat <bekzat@avantage.one>
2023-10-02 18:57:50 +0600

    he

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CreateTask from "./components/CreateTask";
+import { Route, Routes } from "react-router-dom";
+import ListTasks from "./components/ListTasks";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<CreateTask />} />
+          <Route path="/list" element={<ListTasks />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/CreateTask.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateTask = ({tasks, setTasks}: any) => {
+    return (
+        <div>
+            Create
+        </div>
+    );
+};
+
+export default CreateTask;
